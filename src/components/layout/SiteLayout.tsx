759dd64d988ebc930d49622ce9785b3a16338b59
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-19 12:08:15 +0330

    feat: add skip navigation and route announcements

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { RouteAnnouncer } from "@/components/accessibility/RouteAnnouncer";
 import { FloatingWhatsApp } from "@/components/layout/FloatingWhatsApp";
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 
 export const SiteLayout = () => (
-  <div className="flex min-h-screen flex-col">
+  <div className="flex min-h-screen min-w-0 flex-col">
+    <a href="#main-content" className="skip-link">
+      رفتن به محتوای اصلی
+    </a>
+    <RouteAnnouncer />
     <Header />
-    <main id="main-content" className="flex-1 outline-none" tabIndex={-1}>
+    <main
+      id="main-content"
+      className="min-w-0 flex-1 outline-none"
+      tabIndex={-1}
+    >
       <Outlet />
     </main>
     <Footer />
