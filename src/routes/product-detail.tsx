045ece7b6715a5f8035efd471bd693cf3bf708fc
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-22 20:47:29 +0330

    Route product details through public SSR loader

---
+++ src/routes/product-detail.tsx
@@ -0,0 +1,3 @@
+export { loadProductPublicData as loader } from "@/lib/public-loaders.server";
+export { default } from "@/pages/ProductDetailPage";
+export { default as ErrorBoundary } from "@/routes/PublicRouteErrorBoundary";
