694f0286fe33b21f2b97175fc0d2723ce5e3d86c
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-22 20:47:54 +0330

    Route city pages through public SSR loader

---
+++ src/routes/city.tsx
@@ -0,0 +1,3 @@
+export { loadCityPublicData as loader } from "@/lib/public-loaders.server";
+export { default } from "@/pages/CityPage";
+export { default as ErrorBoundary } from "@/routes/PublicRouteErrorBoundary";
