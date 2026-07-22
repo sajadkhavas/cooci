3c8dea6764e263b501a592ae40d9dae60b91380e
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-22 20:47:40 +0330

    Route blog listing through public SSR loader

---
+++ src/routes/blog-list.tsx
@@ -0,0 +1,3 @@
+export { loadBlogListPublicData as loader } from "@/lib/public-loaders.server";
+export { default } from "@/pages/BlogListPage";
+export { default as ErrorBoundary } from "@/routes/PublicRouteErrorBoundary";
