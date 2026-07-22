e3d6c67f92921839bc32e11f444313597fb29a83
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-22 20:47:23 +0330

    Route homepage through public SSR loader

---
+++ src/routes/home.tsx
@@ -0,0 +1,3 @@
+export { loadHomePublicData as loader } from "@/lib/public-loaders.server";
+export { default } from "@/pages/HomePage";
+export { default as ErrorBoundary } from "@/routes/PublicRouteErrorBoundary";
