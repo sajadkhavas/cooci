910953cb1006997c10b7252b76c44e72ee200d52
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-22 20:47:48 +0330

    Route blog details through public SSR loader

---
+++ src/routes/blog-detail.tsx
@@ -0,0 +1,3 @@
+export { loadBlogDetailPublicData as loader } from "@/lib/public-loaders.server";
+export { default } from "@/pages/BlogDetailPage";
+export { default as ErrorBoundary } from "@/routes/PublicRouteErrorBoundary";
